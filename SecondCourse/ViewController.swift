0ff3904faa2d04e17b0b2cc5537906aba487a127
deshollow <555demin@gmail.com>
2023-09-19 10:48:28 +0300

    Initial Commit

---
+++ SecondCourse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondCourse
+//
+//  Created by deshollow on 19.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
